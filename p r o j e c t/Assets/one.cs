a0db124ac1dc8c4286b8ce43f3faa7baf675782d
tuturbik <faratostap@gmail.com>
2023-08-16 00:33:20 +0300

    провокація

--- p r o j e c t/Assets/one.cs
+++ p r o j e c t/Assets/one.cs
@@ -7,7 +7,7 @@ public class one : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        // ще якась чухня
     }
 
     // Update is called once per frame
